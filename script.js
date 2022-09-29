c6a47c2e964af20f506eeb6ca6aca5fced8254dd
TheLudde235 <ludde.lagerstrom@outlook.com>
2022-09-29 12:57:38 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+let saturation = document.querySelector('#saturation');
+
+camera_button.addEventListener('click', async function() {
+   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+	video.srcObject = stream;
+});
+
+click_button.addEventListener('click', function() {
+   	let ctx = saturation.getContext('2d');
+	ctx.drawImage(video, 0, 0, saturation.width, saturation.height);
+	ctx.globalCompositeOperation = 'saturation';
+	ctx.fillStyle = 'hsl(0, 100%, 100%)';
+	ctx.fillRect(0, 0, saturation.width, saturation.height);
+	ctx.globalCompositeOperation = "source-over";
+	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+});
