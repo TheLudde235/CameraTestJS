0c754837f266777c3aebeb1cbc869936e9ae7b5a
TheLudde235 <ludde.lagerstrom@outlook.com>
2022-12-29 15:18:35 +0100

    Added interval to draw frames

--- script.js
+++ script.js
@@ -8,15 +8,28 @@ camera_button.addEventListener('click', async function() {
 	video.srcObject = stream;
 });
 
-click_button.addEventListener('click', function() {
-   	let ctx = saturation.getContext('2d');
-	ctx.drawImage(video, 0, 0, saturation.width, saturation.height);
-	ctx.globalCompositeOperation = 'saturation';
-	ctx.fillStyle = 'hsl(100%, 150%, 100%)';
-	saturation.style.filter = 'contrast(1000000%)';
-	ctx.fillRect(0, 0, saturation.width, saturation.height);
-	ctx.globalCompositeOperation = "source-over";
-	console.trace(ctx)
-	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+function updateFrame() {
+	let h = '100';
+	let s = '100';
+	let l = '100';
+   	let saturationContext = saturation.getContext('2d');
+	saturationContext.drawImage(video, 0, 0, saturation.width, saturation.height);
+	saturationContext.globalCompositeOperation = 'saturation';
+	saturationContext.fillStyle = `hsl(${h}%, ${s}%, ${l}%)`;
+	saturation.style.filter = 'contrast(10000%)';
+	saturationContext.fillRect(0, 0, saturation.width, saturation.height);
+	saturationContext.globalCompositeOperation = "source-over";
+	console.trace(saturationContext);
+	let canvasContext = canvas.getContext('2d');
+	canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+	canvasContext.globalCompositeOperation = 'saturation';
+	canvasContext.fillStyle = `hsl(${h}%, ${s}%, ${l}%)`;
+	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+	canvasContext.globalCompositeOperation = 'source-over';
+}
+setInterval(() => {
+	window.requestAnimationFrame(() => {
+		updateFrame();
+	});
 });
 
